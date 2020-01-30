4612cb04f46dc5bdbb229d6900c0b53384e7e270
ALDO_M <aldo.marukot@gmail.com>
2020-01-30 00:49:03 -0500

    changes to user controller CRUD and also to user model, added profilePicURL field

--- models/User.js
+++ models/User.js
@@ -1,24 +1,23 @@
 const mongoose = require("../db/connection");
 const UserSchema = mongoose.Schema({
-  userID : String,
+  userID: String,
   name: String,
   description: String,
+  profilePicURL: String,
   mylikes: [
     {
-      
       ref: "Post",
       type: mongoose.Schema.Types.ObjectId
     }
   ]
-// likes as models? likes = collection of users who have liked the post.
+  // likes as models? likes = collection of users who have liked the post.
 
-//   postid: [
-//     {
-//       ref: "Post",
-//       type: mongoose.Schema.Types.ObjectId
-//     }
-//   ]
-
+  //   postid: [
+  //     {
+  //       ref: "Post",
+  //       type: mongoose.Schema.Types.ObjectId
+  //     }
+  //   ]
 });
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
